b3c9f14ae9bcaf303de618184c6bbc633e3927bf
0xSuhas <suhasadhav@gmail.com>
2022-03-05 14:07:52 +0530

    Deploy and Creator Test Creation

--- test/Fanbuidl.test.js
+++ test/Fanbuidl.test.js
@@ -1,21 +1,37 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Fanbuidl", function () {
-  const [owner, addr1, addr2] = await ethers.getSigners();
-  const Fanbuidl = await ethers.getContractFactory("Fanbuidl");
-  const fb = await Fanbuidl.deploy();
+let owner;
+let addr1;
+let addr2;
+let fb;
+let Fanbuidl;
 
-  it("Should return the Owner address", async function () {  
+beforeEach(async function () {
+  [owner, addr1, addr2] = await ethers.getSigners();
+  Fanbuidl = await ethers.getContractFactory("Fanbuidl");
+  fb = await Fanbuidl.deploy();
+});
+
+describe("Fanbuidl Deploy", function () {
+  it("Should set and return the Owner address", async function () {  
     // Check address is equal to owner who deployed it
     expect(await fb.getOwner()).to.equal(owner.address);
   });
+});
 
+describe("Fanbuidl Creator", function () {
   it("Should create the Creator", async function () {
+    // Create creator
+    x = await fb.createCreator("Suhas", "This is description for the creator and his/her content and can be long", 2, 10);
+    acc = await fb.getCreator(owner.address);
 
-    x = await fb.createCreator("Suhas", "This is desc", 0, 10);
-
-    // Check address is equal to owner who deployed it
-    expect(x).to.have.deep.property('accountName');
+    // Check all data in creator array
+    expect(acc).to.have.deep.property("accountName", "Suhas");
+    expect(acc).to.have.deep.property("desription", "This is description for the creator and his/her content and can be long");
+    expect(acc).to.have.deep.property("subType", 2);
+    expect(acc).to.have.deep.property("subFee", ethers.BigNumber.from(10));    
+    expect(acc).to.have.deep.property("balance", ethers.BigNumber.from(0));
   });
+  
 });
