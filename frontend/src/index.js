fd94644631d5ac80c7d7a9b0714d3e19c11251f0
0xSuhas <suhasadhav@gmail.com>
2022-03-31 23:34:52 +0530

    Remove React-18 warning

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { createRoot } from 'react-dom/client';
 import './assets/styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
