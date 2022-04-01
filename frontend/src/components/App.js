e6861fd6039df0c8f3f2e9add285d7b95de7b3cf
0xSuhas <suhasadhav@gmail.com>
2022-04-01 10:37:41 +0530

    Frontend Wallet Connect function

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,41 +1,120 @@
-import '../assets/styles/App.css';
-import {Button, Row, Col} from 'react-bootstrap';
-import { useState } from 'react';
+import React from "react";
+import "../assets/styles/App.css";
+import { Button, Row, Col } from "react-bootstrap";
+import { ConnectWallet } from "./ConnectWallet";
+//import { useState, setState } from "react";
 
-function App() {
-  const [currentAccount, setCurrentAccount] = useState(null);
-  const checkWalletIsConnected = () => { 
-    const { ethereum } = window;
-    if(!ethereum){
-      console.log("Wallet not installed");
-    } else{
-      console.log("wallet is present");
-    }
+const NETWORK_ID = "31337";
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.initialState = {
+      selectedAddress: undefined,
+      networkError: undefined,
+    };
+
+    this.state = this.initialState;
   }
-  const connectWalletHandler = async () => {
+
+  _initialize(userAddress) {
+    // This method initializes the dapp
+    this.setState({
+      selectedAddress: userAddress,
+    });
+  }
+
+  // Check if correct network is selected or not
+  _checkNetwork() {
+    if (window.ethereum.networkVersion === NETWORK_ID) {
+      return true;
+    }
+
+    this.setState({
+      networkError: "Please connect your wallet to NETWORK ID: " + NETWORK_ID,
+    });
+
+    return false;
+  }
+
+  // This method resets the state
+  _resetState() {
+    this.setState(this.initialState);
+  }
+
+  // This method just clears part of the state.
+  _dismissNetworkError() {
+    this.setState({ networkError: undefined });
+  }
+
+  async _connectWallet() {
     const { ethereum } = window;
-    if(!ethereum){
-      alert("Install wallet");
-    } else{
-      try{
-        const accounts = await ethereum.request({method: 'eth_requestAccounts'});
-        setCurrentAccount(accounts[0]);
-        console.log("Account address: ", accounts[0])
-      }
-      catch(err){
+
+    if (!ethereum) {
+      alert("No wallet found");
+    } else {
+      try {
+        if (!this._checkNetwork()) {
+          return;
+        }
+        const [selectedAddress] = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        this._initialize(selectedAddress);
+        // We reinitialize it whenever the user changes their account.
+        window.ethereum.on("accountsChanged", ([newAddress]) => {
+          // `accountsChanged` event can be triggered with an undefined newAddress.
+          // This happens when the user removes the Dapp from the "Connected
+          // list of sites allowed access to your addresses" (Metamask > Settings > Connections)
+          // To avoid errors, we reset the dapp state
+          if (newAddress === undefined) {
+            return this._resetState();
+          }
+
+          this._initialize(newAddress);
+        });
+
+        // We reset the dapp state if the network is changed
+        window.ethereum.on("chainChanged", ([networkId]) => {
+          //this._stopPollingData();
+          this._resetState();
+        });
+      } catch (err) {
         console.log(err);
       }
     }
   }
-  return (
-    <div className="container">
-      <Row className="mx-0">
-        <Col xs={5}>
-          <Button variant="outline-primary" onClick={connectWalletHandler} >Connect Wallet</Button>{' '}
-        </Col>
-      </Row>
-    </div>
-  );
+
+  render() {
+    // Ethereum wallets inject the window.ethereum object. If it hasn't been
+    // injected, we instruct the user to install MetaMask.
+    if (window.ethereum === undefined) {
+      console.log("No wallet detected!");
+      //return <NoWalletDetected />;
+    }
+
+    if (!this.state.selectedAddress) {
+      return (
+        <ConnectWallet
+          connectWallet={() => this._connectWallet()}
+          networkError={this.state.networkError}
+          dismiss={() => this._dismissNetworkError()}
+        />
+      );
+    }
+
+    // If everything is loaded, we render the application.
+    return (
+      <div className="container">
+        <Row className="mx-0">
+          <Col xs={5}>
+            <Button variant="outline-primary">Enter App</Button>{" "}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default App;
