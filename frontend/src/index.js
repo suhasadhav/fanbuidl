bb7e91be37660665b4551401be8cc0d2906c8e13
Suhas <suhasadhav@gmail.com>
2022-04-05 21:50:48 -0700

    Auth module include from argon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,25 @@ import "./assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { createRoot } from "react-dom/client";
 import "./assets/styles/index.css";
-import App from "./components/App";
+//import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
 //import { CreatorForm } from "./components/CreatorForm";
 
 import AuthLayout from "./layouts/Auth.js";
 
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<AuthLayout tab="home" />);
+root.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+      <Redirect from="/" to="/auth" />
+    </Switch>
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
