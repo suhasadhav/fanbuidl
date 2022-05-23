0d72a8c72f516f204cb62514aea3c19432d12fcc
Suhas <suhasadhav@gmail.com>
2022-05-24 00:03:31 +0530

    Frontend2

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useState } from "react";
 
 function App() {
+  const [accounts, setAccounts] = useState(0);
+  const { ethereum } = window;
+
+  const isMetaMaskConnected = () => accounts && accounts.length > 0;
+
+  const onClickConnect = async () => {
+    console.log("Called onclickconnect");
+    try {
+      const newAccounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      handleNewAccounts(newAccounts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const checkConnection = async () => {
+    try {
+      const newAccounts = await ethereum.request({
+        method: "eth_accounts",
+      });
+      console.log(newAccounts);
+    } catch (err) {
+      console.error("Error on init when getting accounts", err);
+    }
+  };
+
+  function handleNewAccounts(newAccounts) {
+    setAccounts(newAccounts);
+    console.log(accounts);
+    if (isMetaMaskConnected()) {
+      //initializeAccountButtons();
+    }
+    //updateButtons();
+  }
+  useEffect(() => {
+    checkConnection();
+  }, []);
+
+  //TODO: set initial state of accounts if Metamask is connected
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{accounts}</h2>
+        <a onClick={onClickConnect}>Connect</a>
       </header>
     </div>
   );
